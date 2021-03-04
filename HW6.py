3d1447e8704c6344540a0c18bac8a6b9333d9a77
OSXnov <ricardojrios@outlook.com>
2021-03-03 21:35:55 -0400

    Added the HW6 , pending approval

--- HW6.py
+++ HW6.py
@@ -11,3 +11,67 @@
 # If someone passes a value greater than either maximum, your function should return False.
 
 # Homework Assignment #6: Advanced Loops
+
+
+"""
+Pattern:
+ 01234
+  | |     0
+ -----    1
+  | |     2
+ -----    3
+  | |     4
+"""  
+
+MAX_ROWS = 50
+MAX_COLUMN = 230
+
+def drawPlayingBoard(rows, col):
+    if rows != int or col != int:
+        return False
+
+    if rows <= 1 or col <=1:
+        return False
+
+    if rows > MAX_ROWS or col > MAX_COLUMN:
+        return False
+
+
+    for row in range(0,rows):
+        if row % 2 == 0:
+            for column in range (0, col):
+                if column % 2 == 0:
+                    endChar = ""
+
+                    if column == col - 1:
+                        endChar = "\n"
+                    print(" ", end = endChar)
+
+                else:
+                    print("|", end="")
+
+        
+
+        else :
+
+            for column in range (0, col):
+                endChar = " "
+
+                if column == col - 1:
+                    endChar = "\n"
+
+                print("-", end = endChar)
+
+
+    print("")
+    return True
+
+
+def usingBoardCreated(rows,col):
+    if(drawPlayingBoard(rows, col)):
+        print("Enjoy! Here is your playing board.")
+
+    else:
+        "No playing board for you."
+
+usingBoardCreated(5, 5)
