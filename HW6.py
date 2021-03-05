6f911eedbe3f1df52642ddf490eeda7179d0ada0
OSXnov <ricardojrios@outlook.com>
2021-03-05 16:51:22 -0400

    HW6 Approved

--- HW6.py
+++ HW6.py
@@ -1,18 +1,4 @@
-#                                      Details
-
-# Create a function that takes in two parameters: rows, and columns, both of which are integers. 
-# The function should then proceed to draw a playing board (as in the examples from the lectures) the same number of rows and 
-# columns as specified. After drawing the board, your function should return True.
-
-
-#                                      Extra Credit:
-
-# Try to determine the maximum width and height that your terminal and screen can comfortably fit without wrapping. 
-# If someone passes a value greater than either maximum, your function should return False.
-
-# Homework Assignment #6: Advanced Loops
-
-
+#      2/5/2021
 """
 Pattern:
  01234
