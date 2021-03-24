3b8ca4baa17b7189b30487683d1f09cdaf80910f
OSXnov <ricardojrios@outlook.com>
2021-03-24 00:51:44 -0400

    HW11 approved now adding the final project

---
+++ Projects/CardGame.py
@@ -0,0 +1,15 @@
+# Project #3: Pick a Card Game!
+
+
+# Details:
+ 
+#  Everyone has their favorite card game. What's yours? For this assignment, choose a card game (other than Blackjack),
+#  and turn it into a Python program. It doesn't matter if it's a 1-player game, or a 2 player game, or more! 
+#  That's totally up to you. A few requirements:
+
+# It's got to be a card game (no board games, etc)
+# When the game starts up, you should ask for the players' names. 
+# And after they enter their names, your game should refer to them by name only. ("It's John's turn" instead of "It's player 1's turn). 
+# At any point during the game, someone should be able to type "--help" to be taken to a screen where they can read the rules of the game and instructions for how to play. 
+# After they're done reading, they should be able to type "--resume" to go back to the game and pick up where they left off.
+
